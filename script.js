71bca9d43b5531e710afd3924d45f759c37628ec
pragya <pragyamrinal07@gmail.com>
2017-08-26 17:03:09 +0530

    JS Stalker

---
+++ script.js
@@ -0,0 +1,6 @@
+var firstName= prompt ("what is your first name");
+var lastName= prompt ("what is your last name");
+var age= prompt ("what is your age");
+var fullName= firstName + " "+lastName;
+console.log("your full name is "+ fullName);
+console.log("you are "+age+" "+"years old");
