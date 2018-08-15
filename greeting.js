68804ff1591c923c2c0c536fbd114a8c0cfc3e15
pragyasrivastava <pragyamrinal07@gmail.com>
2018-08-15 14:34:31 +0530

    Exporting functions

---
+++ greeting.js
@@ -0,0 +1,19 @@
+// greeting methods
+var sayHelloInEnglish = function() {
+  return 'Hello'
+}
+
+var sayHelloInSwedish = function() {
+  return 'Hej'
+}
+
+var sayHelloInTatar = function() {
+  return 'Isänme'
+}
+
+console.log('Swedish ' +
+  sayHelloInSwedish() +
+  ' & English ' +
+  sayHelloInEnglish() +
+  ' & Tatar ' +
+  sayHelloInTatar())
